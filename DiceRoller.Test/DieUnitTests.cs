c58e2672073209c1b12d43ba326ed245453d98df
Tri Le <trile95.it@gmail.com>
2022-08-10 20:52:30 -0400

    02:13:45 follow tdd to add unit test methods for die constructors such as testing custom sides and roll method

--- DiceRoller.Test/DieUnitTests.cs
+++ DiceRoller.Test/DieUnitTests.cs
@@ -28,6 +28,49 @@ namespace DiceRoller.Test
             def.NumSides.Should().Be(6);
             def.CurrentSide.Should().BeInRange(1, 6);
         }
+
+        [TestMethod]
+        public void RollSetsSideCorrectly()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                def.Roll();
+                def.CurrentSide.Should().BeInRange(1, 6);
+            }
+        }
+
+        [TestMethod]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(8)]
+        [DataRow(10)]
+        [DataRow(12)]
+        [DataRow(20)]
+        public void DieHasCustomSides(int sides)
+        {
+            Die d = new Die(sides);
+            d.Name.Should().Be("d" + sides);
+            d.NumSides.Should().Be(sides);
+            d.CurrentSide.Should().BeInRange(1, sides);
+        }
+
+        [TestMethod]
+        public void DieHasCustomName()
+        {
+            // not implemented
+        }
+
+        [TestMethod]
+        public void SetSideUpChangesSide()
+        {
+            // not implemented
+        }
+
+        [TestMethod]
+        public void NumSidesShouldNotBeNegative()
+        {
+            // not implemented
+        }
     }
 }
 
--- DiceRoller/DiceRoller/Models/Die.cs
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -14,6 +14,22 @@ namespace DiceRoller.Models
 
         public Die()
         {
+            Name = "d6";
+            NumSides = 6;
+            Roll();
+        }
+
+        public Die(int numSides)
+        {
+            Name = "d" + numSides;
+            NumSides = numSides;
+            Roll();
+        }
+
+        public void Roll()
+        {
+            Random r = new Random();
+            CurrentSide = r.Next(NumSides) + 1;
         }
     }
 }
