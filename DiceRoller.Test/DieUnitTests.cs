c5394fbc073b99c655bb3300fc97c9bafd3f8a23
Tri Le <trile95.it@gmail.com>
2022-08-10 20:12:48 -0400

    01:41:48 follow tdd testing school and add unit tests for the die class logic and make sure they are failed first before actually writing the logic code to make the tests passed create new mstest project to prepare for adding unit test suites for die class logic. Add FluentAssertions nuget package to enable straightforward english-like assertion expressions

---
+++ DiceRoller.Test/DieUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using DiceRoller.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiceRoller.Test
+{
+    [TestClass]
+    public class DieUnitTests
+    {
+        private Die def = new Die();
+
+        [TestMethod]
+        public void DieNotNull()
+        {
+            def.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void DieHasAllDefaultValues()
+        {
+            /*
+             * Default values should be:
+             * name:        d6
+             * numSides:    6
+             * currentSide: ???
+             */
+            def.Name.Should().Be("d6");
+            def.NumSides.Should().Be(6);
+            def.CurrentSide.Should().BeInRange(1, 6);
+        }
+    }
+}
+
---
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DiceRoller.Models
+{
+    public class Die
+    {
+        // name, type, or colour of the die
+        public string Name { get; set; }
+
+        // how many sides are the die
+        public int NumSides { get; set; }
+
+        // which number is currently up
+        public int CurrentSide { get; set; }
+
+        public Die()
+        {
+        }
+    }
+}
+
